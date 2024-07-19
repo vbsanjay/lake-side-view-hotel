30f0db3f4cacb3aacb1c0a3a448f37bdbfea56bc
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-19 14:29:30 +0530

    response files has been updated

---
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/response/BookingResponse.java
@@ -0,0 +1,31 @@
+package com.backend_project.lake_side_view_hotel.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingResponse {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private RoomResponse room;
+
+    public BookingResponse(Long id, LocalDate checkInDate,
+                           LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
