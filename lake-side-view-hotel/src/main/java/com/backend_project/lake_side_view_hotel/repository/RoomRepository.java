6cde131a4daa39f60021b3042caa7dc0b3983544
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-20 16:28:28 +0530

    Room repository updated to fetch distrinct room data

--- lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/repository/RoomRepository.java
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/repository/RoomRepository.java
@@ -2,6 +2,12 @@ package com.backend_project.lake_side_view_hotel.repository;
 
 import com.backend_project.lake_side_view_hotel.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
