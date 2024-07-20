520ca26e92a659743092892ef3f7e789820532c1
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-20 16:21:28 +0530

    add room and get room feature has been updated

--- lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/controller/RoomController.java
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/controller/RoomController.java
@@ -5,15 +5,13 @@ import com.backend_project.lake_side_view_hotel.response.RoomResponse;
 import com.backend_project.lake_side_view_hotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,6 +27,10 @@ public class RoomController {
         RoomResponse response = new RoomResponse(savedRoom.getId(),
                 savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
 
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes(){
+        return roomService.getAllRoomTypes();
     }
 }
