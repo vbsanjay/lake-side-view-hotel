74c224f56acb71bfb9cb5cfec0d2edbf3b516862
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-20 16:19:30 +0530

    Cors Configuration file has been created so that front end and back end works

---
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.backend_project.lake_side_view_hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173"); // Add your frontend URL here
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
