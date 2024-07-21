3b752e368cd64c018b5ebdccdb3461f998c03aa2
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 23:47:57 +0530

    basic admin page design done

--- CLIENT/lakeSide-hotel/src/App.jsx
+++ CLIENT/lakeSide-hotel/src/App.jsx
@@ -10,6 +10,7 @@ import EditRoom from "./components/room/EditRoom"
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
 import RoomListing from './components/room/RoomListing'
+import Admin from './components/admin/Admin'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/existing-rooms" element={<ExistingRooms/>}></Route>
           <Route path="/add-room" element={<AddRoom/>}></Route>
           <Route path="/browse-all-rooms" element={<RoomListing/>}></Route>
+          <Route path="/admin" element={<Admin/>}></Route>
         </Routes>
       </Router>
       <Footer/>
