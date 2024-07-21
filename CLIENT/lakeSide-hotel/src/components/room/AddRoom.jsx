6fa766e746196cbf7a7b60745e6b2e216026f75d
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 22:14:45 +0530

    Back button has been included in webpage

--- CLIENT/lakeSide-hotel/src/components/room/AddRoom.jsx
+++ CLIENT/lakeSide-hotel/src/components/room/AddRoom.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { addRoom } from "../utils/ApiFunctions";
 import RoomTypeSelector from "../common/RoomTypeSelector";
+import { Link } from "react-router-dom";
 
 /* Component declaration */
 const AddRoom = () => {
@@ -127,6 +128,9 @@ const AddRoom = () => {
                             </div>
                             
                             <div className="d-grid d-md-flex mt-2">
+                                <Link to={"/existing-rooms"} className="btn btn-outline-info">
+                                    Back
+                                </Link>
                                 <button className="btn btn-outline-primary ml-5">
                                     Save Room
                                 </button>
