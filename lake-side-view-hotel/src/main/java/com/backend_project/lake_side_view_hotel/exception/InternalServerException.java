7f9635ba548af31ac67e67522fa508222284725f
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 22:12:42 +0530

    Server Error exception added

---
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.backend_project.lake_side_view_hotel.exception;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super (message);
+    }
+}
