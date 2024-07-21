2c2c9b21cca8f2865ff63f77c2d3437878663438
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 22:15:35 +0530

    Navbar and footer is created

--- CLIENT/lakeSide-hotel/src/App.jsx
+++ CLIENT/lakeSide-hotel/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import '../src/App.css'
+// import '../src/App.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom'
@@ -7,18 +7,23 @@ import ExistingRooms from './components/room/ExistingRooms'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./components/home/Home"
 import EditRoom from "./components/room/EditRoom"
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <>
     <main>
       <Router>
+        <NavBar/> 
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/edit-room/:roomId" element={<EditRoom/>}></Route>
           <Route path="/existing-rooms" element={<ExistingRooms/>}></Route>
+          <Route path="/add-room" element={<AddRoom/>}></Route>
         </Routes>
       </Router>
+      <Footer/>
     </main>
     </>
   )
