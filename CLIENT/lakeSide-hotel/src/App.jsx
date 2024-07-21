811911d97a9198028e991c55227afd656bd61fe6
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 23:37:26 +0530

    App.jsx file updated for browser all room web page

--- CLIENT/lakeSide-hotel/src/App.jsx
+++ CLIENT/lakeSide-hotel/src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./components/home/Home"
 import EditRoom from "./components/room/EditRoom"
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/edit-room/:roomId" element={<EditRoom/>}></Route>
           <Route path="/existing-rooms" element={<ExistingRooms/>}></Route>
           <Route path="/add-room" element={<AddRoom/>}></Route>
+          <Route path="/browse-all-rooms" element={<RoomListing/>}></Route>
         </Routes>
       </Router>
       <Footer/>
