2465170d1238900f9a0adbb93ba5fe3621df2cfc
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 23:37:04 +0530

    Browse all room page created combining room, roomCard and RoomListing component

---
+++ CLIENT/lakeSide-hotel/src/components/room/RoomListing.jsx
@@ -0,0 +1,13 @@
+import Room from "./Room"
+
+const RoomListing = () => {
+  return (
+    <div>
+        <section className="bg-light p-2 mb-5 mt-5 shadow">
+            <Room></Room>
+        </section>
+    </div>
+  )
+}
+
+export default RoomListing
