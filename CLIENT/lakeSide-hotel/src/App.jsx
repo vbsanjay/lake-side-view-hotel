46f222f46c4b53055fbd23dda1d2252806bfd9da
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 12:24:38 +0530

    routes were utilised

--- CLIENT/lakeSide-hotel/src/App.jsx
+++ CLIENT/lakeSide-hotel/src/App.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom'
+import ExistingRooms from './components/room/ExistingRooms'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./components/home/Home"
+import EditRoom from "./components/room/EditRoom"
 
 function App() {
   return (
     <>
-      <AddRoom></AddRoom>
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/edit-room/:roomId" element={<EditRoom/>}></Route>
+          <Route path="/existing-rooms" element={<ExistingRooms/>}></Route>
+        </Routes>
+      </Router>
+    </main>
     </>
   )
 }
