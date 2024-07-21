8a803dd20cbd46b6ee89df0a2144fab3c04ee985
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 22:15:14 +0530

    Api function to update room and get room by ID is updated

--- CLIENT/lakeSide-hotel/src/components/utils/ApiFunctions.js
+++ CLIENT/lakeSide-hotel/src/components/utils/ApiFunctions.js
@@ -62,3 +62,23 @@ export async function deleteRoom(roomId){
         throw new Error(`Error deleting room ${error.message}`)
     }
 }
+/*This function update a room by Id */
+export async function updateRoom(roomId, roomData){
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+
+    const response = await api.put(`/rooms/update/${roomId}`, formData)
+    return response
+}
+
+/*This function gets a room by Id */
+export async function getRoomById(roomId){
+    try{
+        const result = await api.get(`/rooms/room/${roomId}`)
+        return result.data
+    }catch(error){
+        throw new Error(`Error fetching room ${error.message}`)
+    }
+}
