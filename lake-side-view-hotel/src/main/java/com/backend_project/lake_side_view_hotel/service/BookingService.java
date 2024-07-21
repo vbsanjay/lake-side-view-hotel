22110ad27f90d64316422202a3a09e4c82381cc2
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-21 12:26:33 +0530

    Booking Service renamed

---
+++ lake-side-view-hotel/src/main/java/com/backend_project/lake_side_view_hotel/service/BookingService.java
@@ -0,0 +1,13 @@
+package com.backend_project.lake_side_view_hotel.service;
+
+import com.backend_project.lake_side_view_hotel.model.BookedRoom;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return null;
+    }
+}
