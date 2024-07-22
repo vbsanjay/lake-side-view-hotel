af5eaea46f8d394d6ebbbde922c68ed312d31aa7
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-22 12:44:01 +0530

    Adding all the other components to form a home page

--- CLIENT/lakeSide-hotel/src/components/home/Home.jsx
+++ CLIENT/lakeSide-hotel/src/components/home/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import MainHeader from '../layout/MainHeader' 
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
+import RoomCarousel from '../common/RoomCarousel'
 
 const Home = () => {
   return (
-    <div>
-        <h2>Welcome to the home page</h2>
-    </div>
+    <section>
+        <MainHeader/>
+        <div className='container'>
+          <RoomCarousel/>
+          <Parallax/>
+          <RoomCarousel/>
+          <HotelService/>
+          <Parallax/>
+          <RoomCarousel/>
+        </div>
+    </section>
   )
 }
 
